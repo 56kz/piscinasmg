aae4a3ed3acd2d19716fc2221052face89b6e8bc
khris <khris56@hotmail.com>
2020-03-11 19:37:12 -0500

    proyects new view created

--- app/controllers/proyects_controller.rb
+++ app/controllers/proyects_controller.rb
@@ -1,5 +1,14 @@
 class ProyectsController < ApplicationController
   def index
+    @proyects = Proyect.all
+  end
+
+  def new
+    @proyect = Proyect.new    
+  end
+
+  private
+  def proyect_params
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'page#index'
 
   get '/proyects', to: 'proyects#index'
+  get '/proyects/new', to: 'proyects#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
