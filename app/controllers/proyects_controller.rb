8d5ae65bcb358d74f07b1e953ef5a1acffb98c1a
Robin Soto Hincapie <andres.soto.h@outlook.com>
2020-03-14 08:54:49 -0500

    cganges in controller and routes

--- app/controllers/proyects_controller.rb
+++ app/controllers/proyects_controller.rb
@@ -3,42 +3,9 @@ class ProyectsController < ApplicationController
     @proyects = Proyect.all
   end
 
-  def new
-    @proyect = Proyect.new
-  end
-
-  def create
-    @proyect = Proyect.new(proyect_params)
-    if @proyect.save
-      redirect_to proyects_path
-    else
-      render :new
-    end
-  end
-
   def show
     @proyect = Proyect.find(params[:id])
     @images=ProjectImage.where(project_name: params[:project_name])
-  end
-
-  def edit
-    @proyect = Proyect.find(params[:id])
-  end
-
-  def update
-    @proyect = Proyect.find(params[:id])
-    if @proyect.update(proyect_params)
-      redirect_to proyects_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    proyect = Proyect.find(params[:id])
-    proyect.destroy
-
-    redirect_to proyects_path      
   end
 
   private
