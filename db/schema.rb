43e124ad2964072184efdd4cd73c15145d54bc34
Andrés Soto <andres.soto.h@outlook.com>
2023-01-12 12:37:45 -0500

    update ruby stack to 2.7 and rails to 7

--- db/schema.rb
+++ db/schema.rb
@@ -2,22 +2,21 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_025328) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_172142) do
   create_table "project_images", force: :cascade do |t|
     t.string "image_name"
     t.string "thumb_name"
     t.string "project_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "proyects", force: :cascade do |t|
@@ -34,8 +33,8 @@ ActiveRecord::Schema.define(version: 2020_03_14_025328) do
     t.text "linkeight"
     t.text "linknine"
     t.text "linkten"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "subtitle"
   end
 
